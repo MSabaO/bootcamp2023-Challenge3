3da2981226ba9bbf29ac2a7f346981db9683cbcf
MaikaSaba <saba.maika@gmail.com>
2023-11-16 20:22:11 -0400

    Fixed prompt now, different combination scenarios added, needed to consult other projects. Not showing password at the end in the console nor the display

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,7 @@ var confirmLower;
 var confirmUpper;
 var confirmNum;
 var confirmSpec;
-var usesrChoices;
+var userChoices;
 
 // lower case letters
 var lowerCase = [
@@ -39,7 +39,7 @@ var lowerCase = [
   "y",
   "z",
 ];
-// to upper case
+// upper case
 var upperCase = [
   "A",
   "B",
@@ -72,7 +72,23 @@ var upperCase = [
 
 // special caracters
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-//var special = ["!", "#", "$", "%", "&", "'", "(",")", "*", "+", ",", "-", ".", "/", "\", "<"];
+var special = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "'",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  "<",
+];
 
 // Write password to the #password input
 function writePassword() {
@@ -83,14 +99,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", testPassword());
-
-function testPassword() {
-  passwordLength = prompt(
-    "How long would you link your password to be? Write in the box a number between 8 and 128"
-  );
-  console.log("Length? " + passwordLength);
-}
+generateBtn.addEventListener("click", generatePassword);
 
 //Function
 function generatePassword() {
@@ -114,11 +123,74 @@ function generatePassword() {
     confirmUpper = confirm("Would you like it to have upper case letters?");
   }
 
-  //
+  //Without response
   if (!confirmSpec && confirmNum && confirmLower && confirmUpper) {
-    usercChoices = alert("A criteria is required");
+    userChoices = alert("A criteria is required");
+    //four choices selected scenario
   } else if (confirmSpec && confirmNum && confirmLower && confirmUpper) {
-    usercChoices = lowerCase.concat(special, numbers, upperCase);
-    console.log(usercChoices);
+    userChoices = lowerCase.concat(special, numbers, upperCase);
+    console.log(userChoices);
   }
+  //three choices selected scenario
+  else if (confirmNum && confirmLower & confirmUpper) {
+    userChoices = lowerCase.concat(upperCase, numbers);
+    console.log(userChoices);
+  } else if (confirmLower && confirmUpper && confirmSpec) {
+    userChoices = lowerCase.concat(upperCase, special);
+    console.log(userChoices);
+  } else if (confirmLower && confirmNum && confirmSpec) {
+    userChoices = lowerCase.concat(numbers, special);
+    console.log(userChoices);
+  } else if (confirmUpper && confirmNum && confirmSpec) {
+    userChoices = upperCase.concat(numbers, special);
+    console.log(userChoices);
+  }
+  //two choices selected scenario
+  else if (confirmLower && confirmNum) {
+    userChoices = lowerCase.concat(upperCase);
+    console.log(userChoices);
+  } else if (confirmLower && confirmNum) {
+    userChoices = lowerCase.concat(numbers);
+    console.log(userChoices);
+  } else if (confirmLower && confirmSpec) {
+    userChoices = lowerCase.concat(special);
+    console.log(userChoices);
+  } else if (confirmUpper && confirmNum) {
+    userChoices = upperCase.concat(numbers);
+    console.log(userChoices);
+  } else if (confirmUpper && confirmSpec) {
+    userChoices = upperCase.concat(special);
+    console.log(userChoices);
+  } else if (confirmNum && confirmSpec) {
+    userChoices = numbers.concat(special);
+    console.log(userChoices);
+  }
+  // just one choice
+  else if (confirmLower) {
+    userChoices = lowerCase;
+    console.log(userChoices);
+  } else if (confirmUpper) {
+    userChoices = blankUpper.concat(upperCase);
+    console.log(userChoices);
+  } else if (confirmNum) {
+    userChoices = numbers;
+    console.log(userChoices);
+  } else if (confirmSpec) {
+    userChoices = special;
+    console.log(userChoices);
+  }
+  //make an empty variable to store password lenght
+  var passwordBlank = [];
+
+  //random
+  for (var i = 0; i < passwordLength; i++) {
+    var allChoices =
+      userChoices[Math.floor(Math.random() * userChoices.lenght)];
+    passwordBlank.push(allChoices);
+    console.log(allChoices);
+  }
+
+  var password = passwordBlank.join("");
+  console.log("Your password is: " + password);
+  return password;
 }
